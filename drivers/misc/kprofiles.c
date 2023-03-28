d75ad037ed1f5d45e96c3c319b798ebc0f5104d4
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2023-03-28 21:05:09 +0530

    drivers/misc/kprofiles  : Set Balanced Mode as Default and Drop AUTO_KP

--- drivers/misc/kprofiles.c
+++ drivers/misc/kprofiles.c
@@ -16,12 +16,12 @@
 #if defined(CONFIG_AUTO_KPROFILES_MSM_DRM) || defined(CONFIG_AUTO_KPROFILES_FB)
 static bool screen_on = true;
 #endif
-static unsigned int mode = 0;
+static unsigned int mode = 2;
 
 static unsigned int override_mode;
 static bool override = false;
 
-static bool auto_kprofiles = true;
+static bool auto_kprofiles = false;
 module_param(auto_kprofiles, bool, 0664);
 module_param(mode, uint, 0664);
 
