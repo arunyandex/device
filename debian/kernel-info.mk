3f4f7527ae5332fe4bdfbd77596c63923fb67884
arunyandex <127389779+arunyandex@users.noreply.github.com>
2023-12-28 04:53:20 +0530

    Update kernel-info.mk

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -2,43 +2,96 @@
 # Kernel settings
 ########################################################################
 
+# Kernel variant. This is currently used only on the Source package name.
+# Use 'android' for Android kernels ("downstream") or 'mainline' for upstream
+# kernels.
 VARIANT = android
+
+# Kernel base version
 KERNEL_BASE_VERSION = 4.14.316
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = bootopt=64S3,32N2,64N2 buildvariant=userdebug
+KERNEL_BOOTIMAGE_CMDLINE = bootopt=64S3,32N2,64N2 androidboot.selinux=permissive androidboot.hardware=rmx2001 buildvariant=eng droidian.lvm.prefer
 
-# Slug for the device vendor. This is going to be used in the KERNELRELEASE
+# Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
 DEVICE_VENDOR = realme
 
 # Slug for the device model. Like above.
 DEVICE_MODEL = rmx6785
 
+# Slug for the device platform. If unsure, keep this commented.
+#DEVICE_PLATFORM = platform
+
 # Marketing-friendly full-name. This will be used inside package descriptions
 DEVICE_FULL_NAME = RealMe 6
 
-# Defconfig to use (adjust this to your actual defconfig file)
+# Whether to use configuration fragments to augment the kernel configuration.
+# If unsure, keep this to 0.
+KERNEL_CONFIG_USE_FRAGMENTS = 0
+
+# Whether to use diffconfig to generate the device-specific configuration.
+# If you enable this, you should set KERNEL_CONFIG_USE_FRAGMENTS to 1.
+# If unsure, keep this to 0.
+KERNEL_CONFIG_USE_DIFFCONFIG = 0
+
+# The diffconfig to apply. Only used when KERNEL_CONFIG_USE_DIFFCONFIG is
+# enabled.
+#KERNEL_PRODUCT_DIFFCONFIG = my_diffconfig
+
+# Defconfig to use
+#KERNEL_DEFCONFIG = nokit_defconfig
 KERNEL_DEFCONFIG = RM6785_defconfig
 
 # Whether to include DTBs with the image. Use 0 (no) or 1.
-KERNEL_IMAGE_WITH_DTB = 1
+KERNEL_IMAGE_WITH_DTB = 0
+
+# Path to the DTB
+# If you leave this undefined, an attempt to find it automatically
+# will be made.
+#KERNEL_IMAGE_DTB = arch/arm64/boot/dts/mediatek/mt6768.dtb
+
+# Whether to include a DTB Overlay. Use 0 (no) or 1.
+KERNEL_IMAGE_WITH_DTB_OVERLAY = 0
+
+# Path to the DTB overlay.
+# If you leave this undefined, an attempt to find it automatically
+# will be made.
+#KERNEL_IMAGE_DTB_OVERLAY = arch/arm64/boot/dts/mediatek/oppo216AF.dtb
+
+# Whether to include the DTB Overlay into the kernel image
+# Use 0 (no, default) or 1.
+# dtbo.img will always be shipped in the linux-bootimage- package.
+KERNEL_IMAGE_WITH_DTB_OVERLAY_IN_KERNEL = 0
+
+# Path to a specifc configuration file for mkdtboimg.
+# The default is to leave it undefined.
+#KERNEL_IMAGE_DTB_OVERLAY_CONFIGURATION = debian/custom_dtbo_config.cfg
+
+# Path to the DTB directory. Only define if KERNEL_IMAGE_DTB_OVERLAY_CONFIGURATION
+# is defined too.
+#KERNEL_IMAGE_DTB_OVERLAY_DTB_DIRECTORY = arch/arm64/boot/dts/qcom
 
 # Various other settings that will be passed straight to mkbootimg
 KERNEL_BOOTIMAGE_PAGE_SIZE = 2048
 KERNEL_BOOTIMAGE_BASE_OFFSET = 0x40078000
 KERNEL_BOOTIMAGE_KERNEL_OFFSET = 0x00008000
-KERNEL_BOOTIMAGE_RAMDISK_OFFSET = 0x07c08000
+KERNEL_BOOTIMAGE_INITRAMFS_OFFSET = 0x07c08000
 KERNEL_BOOTIMAGE_SECONDIMAGE_OFFSET = 0x0
 KERNEL_BOOTIMAGE_TAGS_OFFSET = 0x0bc08000
+
+# Specify boot image security patch level if needed
+#KERNEL_BOOTIMAGE_PATCH_LEVEL = 2022-04-05
+
+# Required for header version 2, ignore otherwise
 KERNEL_BOOTIMAGE_DTB_OFFSET = 0x01E00000
 
-# OS version and patch level
-KERNEL_BOOTIMAGE_OS_VERSION = 13.0.0
-KERNEL_BOOTIMAGE_OS_PATCH_LEVEL = 2023-09
-
 # Kernel bootimage version. Defaults to 0 (legacy header).
-# Your extracted info indicates it's using header version 2
+# As a rule of thumb:
+# Devices launched with Android 8 and lower: version 0
+# Devices launched with Android 9: version 1
+# Devices launched with Android 10: version 2
+# Devices launched with Android 11: version 2 or 3 (GKI)
 KERNEL_BOOTIMAGE_VERSION = 2
 
 ########################################################################
@@ -50,6 +103,11 @@ KERNEL_BOOTIMAGE_VERSION = 2
 # Use 0 (no) or 1 (default).
 DEVICE_VBMETA_REQUIRED = 0
 
+# Samsung devices require a special flag. Enable the following if your
+# device is a Samsung device that requires flag 0 to be present
+# Use 0 (no, default) or 1.
+DEVICE_VBMETA_IS_SAMSUNG = 0
+
 ########################################################################
 # Automatic flashing on package upgrades
 ########################################################################
@@ -60,6 +118,15 @@ FLASH_ENABLED = 1
 # If your device is treble-ized, but aonly, you should set the following to
 # 1 (yes).
 FLASH_IS_AONLY = 1
+
+# `flash-bootimage` defaults are enough for most recent devices, but legacy
+# devices won't work out of the box.
+# If you set the following to 1, this package will set `flash-bootimage`'s
+# DEVICE_IS_AB and BOOTIMAGE_SLOT_A accordingly, thus enabling flashing
+# on older devices.
+#
+# Do not enable if you don't know what you're doing
+FLASH_IS_LEGACY_DEVICE = 0
 
 # Device manufacturer. This must match the `ro.product.vendor.manufacturer`
 # Android property. If you don't want to specify this, leave it undefined,
@@ -77,6 +144,9 @@ FLASH_INFO_MODEL = RMX6785
 # recommended.
 FLASH_INFO_CPU = MT6785
 
+# Space-separated list of supported device ids as reported by fastboot
+#FLASH_INFO_DEVICE_IDS = model1 model2
+
 ########################################################################
 # Kernel build settings
 ########################################################################
@@ -88,12 +158,30 @@ BUILD_CROSS = 1
 # use aarch64-linux-android- if building Android kernels.
 BUILD_TRIPLET = aarch64-linux-android-
 
+# (Cross-build only) The build triplet to use with clang. You'll probably
+# want to use aarch64-linux-gnu- here.
+BUILD_CLANG_TRIPLET = aarch64-linux-gnu-
+
 # The compiler to use. Recent Android kernels are built with clang.
 BUILD_CC = clang
+
+# Extra paths to prepend to the PATH variable. You'll probably want
+# to specify the clang path here (the default).
+BUILD_PATH = /buildd/sources/clang/bin
+
+# Extra packages to add to the Build-Depends section. Mainline builds
+# can have this section empty, unless cross-building.
+# The default is enough to install the Android toolchain, including clang.
+DEB_TOOLCHAIN = linux-initramfs-halium-generic:arm64, binutils-aarch64-linux-gnu, clang-android-6.0-4691093, gcc-4.9-aarch64-linux-android, g++-4.9-aarch64-linux-android, libgcc-4.9-dev-aarch64-linux-android-cross
+
+# Where we're building on
+DEB_BUILD_ON = amd64
+
+# Where we're going to run this kernel on
+DEB_BUILD_FOR = arm64
 
 # Target kernel architecture
 KERNEL_ARCH = arm64
 
 # Kernel target to build
 KERNEL_BUILD_TARGET = Image.gz
-
